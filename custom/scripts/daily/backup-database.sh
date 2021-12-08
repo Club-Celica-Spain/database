3680ae3e373748f9a69ad682437be5edfb6c24bc
Xabi <xaabi6@users.noreply.github.com>
2021-12-08 15:45:40 +0100

    Crear definición del contenedor
    Se han creado los ficheros necesarios para poder desplegar la base de datos en un contenedor Docker.

---
+++ custom/scripts/daily/backup-database.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Options
+BACKUP_FOLDER=/srv/backup/database/mariadb
+NOW=$(date '+%F')
+GZIP=$(which gzip)
+MYSQLDUMP=$(which mysqldump)
+# MariaDB access credentials
+MDB=dbname
+MHOST=hostname
+MPASS=password
+MUSER=username
+
+[ ! -d "$BACKUP_FOLDER" ] && mkdir --parents $BACKUP_FOLDER
+FILE=${BACKUP_FOLDER}/backup-mariadb_${NOW}.sql.gz
+$MYSQLDUMP -h $MHOST -u $MUSER -p${MPASS} --databases $MDB | $GZIP -9 > $FILE
