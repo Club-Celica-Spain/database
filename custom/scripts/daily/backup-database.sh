fae3396b13005c71e620245b19e96700515f485f
Xabi <xaabi6@users.noreply.github.com>
2022-02-15 23:38:52 +0100

    Eliminar copias de seguridad antiguas
    Para no llenar todo el espacio disponible del disco con copias de seguridad, se mantendrán nada más que las últimas siete.

--- custom/scripts/daily/backup-database.sh
+++ custom/scripts/daily/backup-database.sh
@@ -10,6 +10,9 @@ MHOST=hostname
 MPASS=password
 MUSER=username
 
+# Clean old files before generating a new one
+find ${BACKUP_FOLDER} -mindepth 1 -mtime +7 -delete
+
 [ ! -d "$BACKUP_FOLDER" ] && mkdir --parents $BACKUP_FOLDER
 FILE=${BACKUP_FOLDER}/backup-mariadb_${NOW}.sql.gz
 $MYSQLDUMP -h $MHOST -u $MUSER -p${MPASS} --databases $MDB | $GZIP -9 > $FILE
